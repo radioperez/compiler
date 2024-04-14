9f70dd316e520979b59d1aa1637511c48da25428
radioperez <brostikslavik@yandex.com>
2024-04-14 18:12:53 +0700

    hleb

--- main.cpp
+++ main.cpp
@@ -2,35 +2,13 @@
 #include <fstream>
 #include <string>
 
-class WRD {
-public:
-    int read(std::fstream fs, int pos) {
-        char *ch = new char;
-        fs.read(ch, 1);
-        return pos;
-        /*
-        
-        */
-
-    }
-};
-
-class NUM {
-
-};
-
-class OP {
-
-};
-
 int main(){
     char *ch = new char[1];
     std::fstream infile;
     infile.open("input.txt");
     if (infile.is_open()) {
         while (infile.read(ch,1)) {
-            WRD wrd;
-            wrd.read(infile, 0);
+
             /*
             if (*ch < 'z' || *ch > 'a') {
                 // функция чтения слова
